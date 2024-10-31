9c37a87afd77cc08250915bd8171aa5011f0ebc0
satawat <e59704156@gmail.com>
2024-10-31 15:26:35 +0700

    Revert "re fontsize stat"
    This reverts commit 46ecd1637152e1bc9c6b08b0a13c442273b6d585.

--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -6,19 +6,19 @@ function Stat() {
         <div className="skeleton bg-zinc-700 stats shadow w-full rounded-none text-white">
   <div className="stat place-items-center">
     <div className="stat-title text-yellow-400">Room Price</div>
-    <div className="stat-value text-2xl">2,000</div>
+    <div className="stat-value">2,000</div>
     <div className="stat-desc text-yellow-400">ค่าห้อง/เดือน</div>
   </div>
 
   <div className="stat place-items-center">
     <div className="stat-title text-white">New Rooms</div>
-    <div className="stat-value text-2xl text-info text-yellow-400">2,500</div>
+    <div className="stat-value  text-info text-yellow-400">2,500</div>
     <div className="stat-desc text-secondary">↗︎ Complete</div>
   </div>
 
   <div className="stat place-items-center">
     <div className="stat-title text-yellow-400">Maintanance</div>
-    <div className="stat-value text-2xl text-red-600">500</div>
+    <div className="stat-value text-red-600">500</div>
     <div className="stat-desc text-yellow-400">↘︎ ค่าบำรุงรักษา</div>
   </div>
 </div>
